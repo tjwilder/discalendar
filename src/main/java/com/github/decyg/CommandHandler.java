5be2d173069b323bb1880c7ab0073827748c3697
AlexanderPawlowski <43893473+AlexanderPawlowski@users.noreply.github.com>
2018-11-10 18:27:59 -0600

    Removed the audio player

--- src/main/java/com/github/decyg/CommandHandler.java
+++ src/main/java/com/github/decyg/CommandHandler.java
@@ -1,19 +1,8 @@
 package com.github.decyg;
-
-import com.github.decyg.lavaplayer.GuildMusicManager;
-import com.github.decyg.lavaplayer.TrackScheduler;
-import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
-import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
-import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
-import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
-import sx.blah.discord.handle.obj.IVoiceChannel;
 import sx.blah.discord.util.EmbedBuilder;
 import sx.blah.discord.util.RequestBuffer;
 
@@ -27,68 +16,9 @@ public class CommandHandler {
     // A static map of commands mapping from command string to the functional impl
     private static Map<String, Command> commandMap = new HashMap<>();
 
-    private static final AudioPlayerManager playerManager = new DefaultAudioPlayerManager();;;
-    private static final Map<Long, GuildMusicManager> musicManagers  = new HashMap<>();;
-
     // Statically populate the commandMap with the intended functionality
     // Might be better practise to do this from an instantiated objects constructor
     static {
-
-        AudioSourceManagers.registerRemoteSources(playerManager);
-        AudioSourceManagers.registerLocalSource(playerManager);
-
-        // If the IUser that called this is in a voice channel, join them
-        commandMap.put("joinvoice", (event, args) -> {
-
-            IVoiceChannel userVoiceChannel = event.getAuthor().getVoiceStateForGuild(event.getGuild()).getChannel();
-
-            if(userVoiceChannel == null)
-                return;
-
-            userVoiceChannel.join();
-
-        });
-
-        commandMap.put("leavevoice", (event, args) -> {
-
-            IVoiceChannel botVoiceChannel = event.getClient().getOurUser().getVoiceStateForGuild(event.getGuild()).getChannel();
-
-            if(botVoiceChannel == null)
-                return;
-
-            TrackScheduler scheduler = getGuildAudioPlayer(event.getGuild()).getScheduler();
-            scheduler.getQueue().clear();
-            scheduler.nextTrack();
-
-            botVoiceChannel.leave();
-
-        });
-
-        // Plays the first song found containing the first arg
-        commandMap.put("playsong", (event, args) -> {
-
-            IVoiceChannel botVoiceChannel = event.getClient().getOurUser().getVoiceStateForGuild(event.getGuild()).getChannel();
-
-            if(botVoiceChannel == null) {
-                BotUtils.sendMessage(event.getChannel(), "Not in a voice channel, join one and then use joinvoice");
-                return;
-            }
-
-            // Turn the args back into a string separated by space
-            String searchStr = String.join(" ", args);
-
-            loadAndPlay(event.getChannel(), searchStr);
-
-
-        });
-
-        // Skips the current song
-        commandMap.put("skipsong", (event, args) -> {
-
-            skipTrack(event.getChannel());
-
-        });
-
         commandMap.put("exampleembed", (event, args) -> {
 
             EmbedBuilder builder = new EmbedBuilder();
@@ -122,68 +52,6 @@ public class CommandHandler {
 
         });
 
-    }
-
-    private static synchronized GuildMusicManager getGuildAudioPlayer(IGuild guild) {
-        long guildId = guild.getLongID();
-        GuildMusicManager musicManager = musicManagers.get(guildId);
-
-        if (musicManager == null) {
-            musicManager = new GuildMusicManager(playerManager);
-            musicManagers.put(guildId, musicManager);
-        }
-
-        guild.getAudioManager().setAudioProvider(musicManager.getAudioProvider());
-
-        return musicManager;
-    }
-
-    private static void loadAndPlay(final IChannel channel, final String trackUrl) {
-        GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
-
-        playerManager.loadItemOrdered(musicManager, trackUrl, new AudioLoadResultHandler() {
-            @Override
-            public void trackLoaded(AudioTrack track) {
-                BotUtils.sendMessage(channel, "Adding to queue " + track.getInfo().title);
-
-                play(musicManager, track);
-            }
-
-            @Override
-            public void playlistLoaded(AudioPlaylist playlist) {
-                AudioTrack firstTrack = playlist.getSelectedTrack();
-
-                if (firstTrack == null) {
-                    firstTrack = playlist.getTracks().get(0);
-                }
-
-                BotUtils.sendMessage(channel, "Adding to queue " + firstTrack.getInfo().title + " (first track of playlist " + playlist.getName() + ")");
-
-                play(musicManager, firstTrack);
-            }
-
-            @Override
-            public void noMatches() {
-                BotUtils.sendMessage(channel, "Nothing found by " + trackUrl);
-            }
-
-            @Override
-            public void loadFailed(FriendlyException exception) {
-                BotUtils.sendMessage(channel, "Could not play: " + exception.getMessage());
-            }
-        });
-    }
-
-    private static void play(GuildMusicManager musicManager, AudioTrack track) {
-
-        musicManager.getScheduler().queue(track);
-    }
-
-    private static void skipTrack(IChannel channel) {
-        GuildMusicManager musicManager = getGuildAudioPlayer(channel.getGuild());
-        musicManager.getScheduler().nextTrack();
-
-        BotUtils.sendMessage(channel, "Skipped to next track.");
     }
 
     @EventSubscriber
