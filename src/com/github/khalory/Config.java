93f042aaea4cf28559667b2e1033219416e4b9cf
AlexanderPawlowski <43893473+AlexanderPawlowski@users.noreply.github.com>
2019-08-22 16:11:04 -0500

    trying to remove the file with the API key

--- src/com/github/khalory/Config.java
+++ src/com/github/khalory/Config.java
@@ -2,35 +2,35 @@ package com.github.khalory;
 
 // Handles general configuration options
 class Config {
-	
-	// What's the current log level?
-	public static final LogLevel LOG_LEVEL = LogLevel.Debug;
 
-	// Pretty sure there's an easier way to do this enum
-	// comparison but I can't look it up right now
-	public static boolean logDebug() {
-		return LOG_LEVEL == LogLevel.Debug;
-	}
+    // What's the current log level?
+    public static final LogLevel LOG_LEVEL = LogLevel.Debug;
 
-	public static boolean logInfo() {
-		return LOG_LEVEL == LogLevel.Debug
-			|| LOG_LEVEL == LogLevel.Info;
-	}
+    // Pretty sure there's an easier way to do this enum
+    // comparison but I can't look it up right now
+    public static boolean logDebug() {
+        return LOG_LEVEL == LogLevel.Debug;
+    }
 
-	public static boolean logError() {
-		return LOG_LEVEL == LogLevel.Debug
-			|| LOG_LEVEL == LogLevel.Info
-			|| LOG_LEVEL == LogLevel.Error;
-	}
+    public static boolean logInfo() {
+        return LOG_LEVEL == LogLevel.Debug
+                || LOG_LEVEL == LogLevel.Info;
+    }
+
+    public static boolean logError() {
+        return LOG_LEVEL == LogLevel.Debug
+                || LOG_LEVEL == LogLevel.Info
+                || LOG_LEVEL == LogLevel.Error;
+    }
 
 
-	// TODO: Eventually load config from separate files for dev, prod, etc.
+    // TODO: Eventually load config from separate files for dev, prod, etc.
 }
 
 // Possible LogLevels, None for no output
 enum LogLevel {
-	Debug,
-	Info,
-	Error,
-	None;
+    Debug,
+    Info,
+    Error,
+    None;
 }
